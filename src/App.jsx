16cda92a846279dab0916e64ca0465dc21563c0a
Aldo Álvarez Jurado <gacl080711hmcrhsa8@soycecytem.mx>
2026-07-22 15:35:53 -0600

    Persona B: cambia el titulo a version B

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   return (
     <div>
       <Encabezado
-        titulo="Mi primera app en React - VERSION B"
+        titulo="Mi primera app en React - VERSION B - VERSION B"
         subtitulo="Proyecto configurado correctamente."
       />
 
